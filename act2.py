49a01eb2a232ac0ed43218aee03bcd001f64bb70
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-05-10 18:45:01 -0300

    Create act2.py

---
+++ act2.py
@@ -0,0 +1,7 @@
+Cristales_preciosos = ["ruby", "ruby","zafiro","lapizlasuli","lapislazuli"]
+print("Los cristales preciosos encontrados son:", Cristales_preciosos)
+Inventario = ["ruby", "ruby","zafiro","lapizlasuli","lapislazuli","cuarzo","Diamantes"]
+print("Eliminando los cristales repetidos...")
+Inventario.remove("ruby")
+Inventario.remove("lapislazuli")
+print("tu inventario es:", Inventario)
