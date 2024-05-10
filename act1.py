d9623c70cf3d2cfd81d3db9de6ea6b33617bdb33
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-05-10 18:44:36 -0300

    Create act1.py

---
+++ act1.py
@@ -0,0 +1,12 @@
+monedas= [10, 20, 30, 40, 50]
+
+suma_monedas= sum(monedas)
+
+promedio= suma_monedas / len(monedas)
+
+mayor= max(monedas)
+menor= min(monedas)
+
+print("la suma total de las monedas es:", suma_monedas)
+print("El promedio de las monedas es:", promedio)
+print("La moneda mas valiosa es:", mayor)
